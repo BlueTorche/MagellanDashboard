fcf094a0eaa44c7cab5820563e41253fad55a5cc
BlueTorche <dubrulle_kevin@hotmail.be>
2024-10-04 15:41:52 +0200

    Improve README & SQLquery in dashboard.py

--- models/routes/dashboard.py
+++ models/routes/dashboard.py
@@ -1,16 +1,33 @@
 from flask_apispec import MethodResource
 from flask import render_template, make_response, request, redirect, url_for
 
+from sqlalchemy import func
+
 from models.database.databasemodel import Logs, db
 
 class Dashboard(MethodResource):
     def load_get_method(self):
+        latest_log = db.session.query(
+            Logs.computer,
+            func.max(Logs.date).label('latest_date')
+        ).group_by(Logs.computer).subquery()
 
-        all_logs = Logs.query.all()
+        last_logs = db.session.query(Logs).join(
+            latest_log,
+            (Logs.computer == latest_log.c.computer) & (Logs.date == latest_log.c.latest_date)
+        ).all()
 
-        print(all_logs)
+        # all_logs = Logs.query.all()
+        #
+        # last_login_log = {}
+        # for log in all_logs:
+        #     computer = log.computer
+        #     if computer in last_login_log:
+        #         if last_login_log[computer]["date"] > log.date:
+        #             break
+        #     last_login_log[computer] = {"date": log.date, "user": log.user}
 
-        template = render_template('dashboard.html', all_logs=all_logs)
+        template = render_template('dashboard.html', all_logs=last_logs)
         response = make_response(template)
         response.headers['Content-Type'] = 'text/html'
         return response
